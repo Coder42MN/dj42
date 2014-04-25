0cf3ce77222da0dd9b6be89b4c485ceade520d53
Erik Braegelmann <ebraegelmann@gmail.com>
2014-04-24 19:29:41 -0500

    Added Gosu for sound playback.

--- parser.rb
+++ parser.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'gosu'
+
 # Usage example: $ ./parser.rb /Path/to/logfile/log.txt
 
 # Here's an array of strings that we'll be searching for in a text file
@@ -12,6 +14,10 @@ $noises = {
  	"BLC::" => "./assets/soundclips/41__elmomo__exprsynths/715__elmomo__rstfinalexprr.mp3",
  	"MW::" => "./assets/soundclips/17__tictacshutup__studio-drums-1/442__tictacshutup__prac-snare-rimshot-2.wav"
  	}
+
+#make some gosu sample objects from our noises so we can play them back later
+$gosu_noises = {}
+$noises.map {|k, v|  $gosu_noises[k] = Gosu::Sample.new(v)}
 
 # Basically, tail a file
 def parse(file, pattern)
@@ -26,7 +32,9 @@ def parse(file, pattern)
     pattern.each {|x|
     	# check and see if it's found in the line
     	# If the element is found, put the value from the noises hash
-    	puts $noises[x] if /#{x}/ =~ line
+    	puts x if /#{x}/ =~ line
+      #and make the corresponding noise with gosu
+      $gosu_noises[x].play if /#{x}/ =~ line
     }
   end
 end
